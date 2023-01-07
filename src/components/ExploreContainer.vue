80abc1f7d47fa9bb9dd7b9b9bb2ed4e5142b1c35
MartinPessoa <desenvolvedor_pessoa@outlook.com>
2023-01-07 01:07:03 -0300

    primeira versão adicionando removendo ingredientes

--- src/components/ExploreContainer.vue
+++ src/components/ExploreContainer.vue
@@ -1,14 +1,85 @@
 <template>
   <div id="container">
-    <strong>{{ name }}</strong>
-    <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+   <ion-text v-if="erro" color="danger" class="ion-color-danger">
+     {{ textoErro }}
+   </ion-text>
+    <ion-list :inset="true">
+      <ion-item v-for="item in itens" :key="item.Id">
+      <span>
+        {{ item.Quantidade }} {{ item.UnidadeMedida }}
+      </span>
+        &nbsp;
+        <span>
+        {{ item.Ingrediente }}
+      </span>
+        <ion-button slot="end"  size="small" color="danger" @click.prevent="Remover(item)">X</ion-button>
+      </ion-item>
+    </ion-list>
+
+
+    <ion-input placeholder="Quantidade" v-model="itemAtual.Quantidade"></ion-input>
+    <ion-input placeholder="Unidade de Medida" v-model="itemAtual.UnidadeMedida"></ion-input>
+    <ion-input placeholder="Ingrediente" v-model="itemAtual.Ingrediente"></ion-input>
+    <ion-button @click.prevent="novoItem">Adicionar</ion-button>
   </div>
 </template>
 
 <script setup lang="ts">
-defineProps({
-  name: String,
-});
+import {IonButton, IonInput, IonList, IonItem, IonText} from "@ionic/vue"
+import {ref, reactive} from "vue";
+
+class Item {
+  Id: number;
+  Quantidade: string;
+  UnidadeMedida: string;
+  Ingrediente: string;
+
+  constructor(id:number, qtd:string, unidade:string, ingrediente:string) {
+    this.Id = id;
+    this.Quantidade = qtd;
+    this.UnidadeMedida = unidade;
+    this.Ingrediente = ingrediente;
+  }
+}
+
+let ultimoId = 0;
+
+const itemAtual = ref(new Item(ultimoId++, "", "", ""))
+const itens = reactive(new Array<Item>());
+const erro = ref(false)
+const textoErro = ref("")
+
+const novoItem = function(){
+  erro.value = false;
+  textoErro.value = ""
+
+  const num = parseFloat(itemAtual.value.Quantidade);
+
+  //console.log(isNaN(num))
+
+  if(isNaN(num)) {
+    erro.value = true;
+    textoErro.value = "Por favor, digite um número válido no campo Quantidade."
+    return
+  }
+
+  if(itemAtual.value.Quantidade)
+
+  itens.push(itemAtual.value);
+
+  console.log(itens)
+
+  itemAtual.value = new Item(ultimoId++, "", "", "")
+}
+
+const Remover = function(item:Item){
+  const index = itens.indexOf(item);
+
+  if(index > -1){
+    itens.splice(index, 1)
+  }
+}
+
 </script>
 
 <style scoped>
